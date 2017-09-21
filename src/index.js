24db868b369d2261668f8f5f236b91151ad5d8fc
iammario <mariodods@gmail.com>
2017-09-21 12:38:22 +0200

    instaliro npm youtube

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from "react";
+import  ReactDOM from "react-dom";
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY=AIzaSyDeo97OtFkyZOz-YUJLDP0Cs2gIexoRXR4;
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+//create a new component. This component should produce some HTML
+
+const App=() =>{
+    return <div>Hi!</div>;
+}
+
+// Take this components generated HTML and put it on the page(in the DOM)
+
+ReactDOM.render(<App />, document.querySelector(".container"));
